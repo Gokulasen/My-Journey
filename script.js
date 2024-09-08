561af023ab5c61d455acd55f9bec616e55e7c2e2
Gokulasen <dragonmater222@gmail.com>
2024-09-08 17:21:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent successfully! We will get back to you soon.');
+});
